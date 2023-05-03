eedfcbebf839c5ad42ec6eb98ee8edb21532db72
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2023-05-03 15:55:39 -0700

    Add CombinedReportParameters.cardSelectionMessageCreator

--- src/reports/package/accessibilityInsightsReport.d.ts
+++ src/reports/package/accessibilityInsightsReport.d.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 import * as axe from 'axe-core';
+import { CardSelectionMessageCreator } from 'common/message-creators/card-selection-message-creator';
 
 declare namespace AccessibilityInsightsReport {
     export type Report = {
@@ -124,6 +125,7 @@ declare namespace AccessibilityInsightsReport {
         browserResolution: string,
         scanDetails: ScanSummaryDetails,
         results: CombinedReportResults,
+        cardSelectionMessageCreator?: CardSelectionMessageCreator,
     }
 
     export type Reporter = {
